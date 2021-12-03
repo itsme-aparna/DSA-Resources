adf67e6dfd57b6056e34abc7b43a79c71732546e
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2021-12-03 13:02:07 +0530

    Time: 12 ms (65.60%), Space: 21.5 MB (99.25%) - LeetHub

---
+++ validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         return valid(root, LONG_MIN, LONG_MAX);
+//     }
+// private:
+//     bool valid(TreeNode* root, long min, long max) {
+//         if (!root) {
+//             return true;
+//         }
+//         if (root -> val <= min || root -> val >= max) {
+//             return false;
+//         }
+//         return valid(root -> left, min, root -> val) && valid(root -> right, root -> val, max);
+//     }
+// };
+
+class Solution {
+public:
+    
+    bool dfs(TreeNode* root, long min, long max){
+        if(root==NULL){
+            return true;
+        }
+        
+        if(root->val <= min || root->val >=max){
+            return false;
+        }
+        
+        return dfs(root->left, min, root->val) && dfs(root->right, root->val, max);
+    }
+    
+    bool isValidBST(TreeNode* root) {
+         if(root==NULL){
+            return true;
+        }
+        return dfs(root, LONG_MIN, LONG_MAX);
+    }
+};
