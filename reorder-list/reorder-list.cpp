efc196abc425db5f5333669d3d3151603a1f6c06
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2021-12-22 19:54:11 +0530

    Time: 1052 ms (5.01%), Space: 17.8 MB (51.58%) - LeetHub

--- reorder-list/reorder-list.cpp
+++ reorder-list/reorder-list.cpp
@@ -12,19 +12,15 @@ class Solution {
 public:
     void reorderList(ListNode* head) {
         ListNode* penultimate;
-        while(head && head->next && head->next->next){
+        while(head&& head->next&&head->next->next){ 
             penultimate = head;
-            while(penultimate->next->next){
-                penultimate = penultimate->next;
-            }
-            penultimate->next->next = head->next;
-            head->next = penultimate->next;
-            
-            penultimate->next = NULL;
-            head = head->next->next;
-            
+        while(penultimate->next->next){
+            penultimate = penultimate->next;
         }
+        penultimate->next->next = head->next;
+        head->next = penultimate->next;
+        penultimate->next = NULL;
+        head = head->next->next;
     }
+}
 };
-
-
