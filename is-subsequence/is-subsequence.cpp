299c856160d3acc9c742e0c712791024a34adda7
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2021-12-25 17:37:30 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (88.53%) - LeetHub

---
+++ is-subsequence/is-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j])
+                i++;
+            j++;
+        }
+        if(i==s.size()){
+            return 1;
+        }
+        return 0;
+    }
+};
