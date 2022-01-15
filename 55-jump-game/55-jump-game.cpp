e23546fb54bda5798bcb4a65a45a5a68ffc9cae0
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-01-15 12:47:29 +0530

    Time: 52 ms (92.26%), Space: 48.4 MB (49.17%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int reach = 0;
+        for(int i=0; i<=reach; i++){
+            reach = max(reach, nums[i]+i);
+            if(reach>= n-1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
