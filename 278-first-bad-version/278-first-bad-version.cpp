a27ac6d46dea62b8b09f0d2ffb5217772a3326ca
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-01-23 18:23:18 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (21.74%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long l = 0;
+        long long r = n;
+        while(l<=r){
+            long long mid = l + (r-l)/2;
+            if(isBadVersion(mid)){
+                r= mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+};
