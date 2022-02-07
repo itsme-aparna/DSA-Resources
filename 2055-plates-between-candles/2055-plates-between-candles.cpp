a370215883145078f8afdc4cffae5d8ebafba6d5
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-02-07 22:21:02 +0530

    Time: 772 ms (29.08%), Space: 140.6 MB (73.55%) - LeetHub

---
+++ 2055-plates-between-candles/2055-plates-between-candles.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
+        vector<int> ans, A;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='|')
+                A.push_back(i);
+            
+        }
+        for(int q=0; q<queries.size(); q++){
+            int i = lower_bound(A.begin(), A.end(), queries[q][0]) - A.begin();
+            int j = upper_bound(A.begin(), A.end(), queries[q][1]) - A.begin()-1;
+            ans.push_back(i<j ? (A[j]-A[i])-(j-i) : 0);
+        }
+        return ans;
+    }
+};
