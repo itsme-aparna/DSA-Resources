28cac73b952f76b1c05eafeb0a528ae7691d2fbf
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-04-19 03:16:45 +0530

    Time: 120 ms (12.80%), Space: 82 MB (5.31%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int func(int i, int j, string word1, string word2, vector<vector<int>> &dp){
+        if(i<0){
+            return j+1;
+        }
+        if(j<0)
+            return i+1;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        if(word1[i] == word2[j])
+            return dp[i][j] = func(i-1, j-1, word1, word2, dp);
+        return dp[i][j] = 1 + min(func(i-1, j, word1, word2, dp), min(func(i, j-1, word1, word2, dp), func(i-1, j-1, word1, word2, dp)));
+                              }
+    
+    int minDistance(string word1, string word2) {
+        // 3 possibilities, check for all of them and take the min of them
+        // if word1 gets exhausted, return word2.length and viceversa
+        
+        int n = word1.size();
+        int m = word2.size();
+        
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return func(n-1, m-1, word1, word2, dp);
+        
+        
+    }
+};
