9f3df0f8ac0c995c29820ca9614f9c9491b1624f
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-04-23 01:34:19 +0530

    Time: 220 ms (39.44%), Space: 52.1 MB (88.06%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -0,0 +1,78 @@
+class MyHashMap {
+    list< pair<int, int> > *l;
+    int size;
+    
+    int calcHash(int key)
+    {
+        return (key % size);
+    }
+public:
+    
+    MyHashMap() {
+        size = 100;
+        l = new list< pair<int, int> >[size];
+    }
+
+    
+    void put(int key, int value) {
+    
+            int k = calcHash(key);
+        
+            auto it = l[k].begin();
+        
+            while(it != l[k].end())
+            {
+                if(it->first == key)
+                {
+                    it->second = value;
+                    return;
+                }
+                it++;
+            }
+        
+            l[k].push_back({key, value});
+        
+    }
+   
+    int get(int key) {
+        
+        int k = calcHash(key);
+        
+        auto it = l[k].begin();
+        
+        while(it != l[k].end())
+        {
+            if(it->first == key)
+                return it->second;
+            it++;
+        }
+    
+        return -1;
+        
+    }
+    
+    void remove(int key) {
+        
+        int k = calcHash(key);
+        
+        auto it = l[k].begin();
+        
+        while(it != l[k].end())
+        {
+            if(it->first == key)
+            {
+               l[k].erase(it);
+               break;
+            }
+            
+            it++;
+        }
+    }
+};
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
