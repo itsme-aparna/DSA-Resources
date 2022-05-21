ab83cbf92b0eb3c248637b0dd47e8cb9503d626c
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-05-21 22:48:23 +0530

    Time: 423 ms (6.64%), Space: 89.4 MB (5.05%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
     
-    int recur(vector<int>& coins, int amount, int idx, vector<vector<int>>& memo){
-        if(idx==0 || amount== 0){
-            return amount==0 ? 0 : INT_MAX-1;
-        }
-        if(memo[amount][idx]!=-1){
-            return memo[amount][idx];
-        }
-        if(amount >= coins[idx-1]){
-            //both
-            return memo[amount][idx] = min((1+recur(coins, amount-coins[idx-1], idx, memo)), recur(coins, amount, idx-1, memo));
-        }
-        else{
-            //exlude
-            return memo[amount][idx] = recur(coins, amount, idx-1, memo);
-        }
+    int solve(vector<int>& coins, int amount, int idx, vector<vector<int> > &memo){
+       if(amount<=0  || idx >= coins.size()){
+           if(amount == 0){
+               return 0;
+           }
+           else{
+               return INT_MAX-1;
+           }
+           
+       }
+        
+       if(memo[amount][idx] != -1){
+           return memo[amount][idx];
+       }
+        
+       
+        
+        if(amount >= coins[idx])
+            return memo[amount][idx] = min( 1+ solve(coins, amount- coins[idx], idx, memo), solve(coins, amount, idx+1, memo));
+        
+        else
+            return memo[amount][idx] = solve(coins, amount, idx+1, memo);
     }
     
     int coinChange(vector<int>& coins, int amount) {
-        int ans;
-        //include or exclude
+        // try all combinations
+        // write the recursive code first - done
+        // memoize
         
-        int n = coins.size();
-        vector<vector<int>> memo(amount+1, vector<int>(n+1, -1));
-        
-        ans = recur(coins, amount, coins.size(), memo);
-        if(ans==INT_MAX-1){
+         vector<vector<int> > memo(amount + 1, vector<int> (coins.size(), -1));
+        if(solve(coins, amount, 0, memo)== INT_MAX-1){
             return -1;
         }
-        return ans;
+        return solve(coins, amount, 0, memo);
     }
 };
