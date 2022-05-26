03d8fdd006e63cfae15dc30875896e80b37607a6
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-05-26 15:21:05 +0530

    Time: 423 ms (69.56%), Space: 10.3 MB (69.02%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    
-    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& memo){
-        if(i>j)
-            return 0;
-        if(memo[i][j]!=-1){
-            return memo[i][j];
-        }
-        int maxi = INT_MIN;
-        for(int k = i; k<=j; k++){
-            int cost = nums[i-1]*nums[k]*nums[j+1] + solve(i, k-1, nums, memo) + solve(k+1, j, nums, memo);
-            maxi = max(maxi, cost);
-        }
-        
-        return memo[i][j] = maxi;
-    }
-    
+   
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
-        vector<vector<int>> memo(n+1, vector<int> (n+1, -1));
-        return solve(1, n, nums, memo);
+        vector<vector<int>> dp(n+2, vector<int> (n+2, 0));
+        
+        for(int i= n; i>=1; i--){
+            for(int j = 1; j<=n; j++){
+                if(i>j) continue;
+                int maxi = INT_MIN;
+                for(int k = i; k<=j; k++){
+                    int cost = nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                    maxi = max(maxi, cost);
+                }
+
+                dp[i][j] = maxi;
+            }
+                
+        }
+        
+        return dp[1][n];
     }
 };
