8119a78cd16788aaa2b90ccd79d072043c13b1c7
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-05-26 17:01:01 +0530

    Time: 866 ms (5.24%), Space: 6.3 MB (98.30%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -9,23 +9,6 @@ public:
         return true;
     }
     
-    int solve(int i, int j, string& s, vector<int>& memo){
-        if(i==j)
-            return 0;
-        if(memo[i]!= -1)
-            return memo[i];
-        int mini = INT_MAX;
-        for(int k = i; k < j; k++){
-            if(isPalindrome(i, k, s)){
-                int cost = 1 + solve(k+1, j, s, memo);
-                mini = min(mini, cost);
-            }
-                
-        }
-        
-        return memo[i] = mini;
-    }
-    
     int minCut(string s) {
         int n = s.length();
         vector<int> dp(n+1, 0);
