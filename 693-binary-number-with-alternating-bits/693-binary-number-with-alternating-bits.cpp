39beae16c9ebb8df81d6f8188ebc3908cd5c679a
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-05-29 01:47:26 +0530

    Time: 6 ms (14.33%), Space: 5.8 MB (90.31%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        int x =n&1;
+        int x =n&1; // here x is equal to the value at last bit of n
         n>>=1;
         while(n){
-            if((n&1)!=x){
-                x = n&1;
-                n>>=1;
+            if((n&1)!=x){ // if its not equal then alternating bits are present
+                x = n&1; // update x with the last bit of n
+                n>>=1; // right shift/ move to prev bit 
             }
             else return false;
         }
