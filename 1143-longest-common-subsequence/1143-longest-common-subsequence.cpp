0de082d72e9f3884b8f8782715c9a65069762232
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-05-31 15:37:54 +0530

    Time: 44 ms (24.51%), Space: 13.3 MB (10.44%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        // tabulation method with space optimization
-        
-        int n = text1.size();
-        int m = text2.size();
-        
-        vector<int> curr(m+1, 0), prev(m+1, 0);
-        
-        for(int i=1; i<=n; i++){
-            for(int j = 1; j<=m; j++){
-                if(text1[i-1] == text2[j-1]){
-                    curr[j] = 1+ prev[j-1];
-                }
-                
-                else{
-                    curr[j] = max(curr[j-1], prev[j]);
-                }
-            }
-            prev = curr;
+    int solve(string& text1, string& text2, int n, int m, vector<vector<int>>& dp){
+        if(n<0 || m<0){
+            return 0;
+        }
+        if(dp[n][m] != -1){
+            return dp[n][m];
         }
         
-        return prev[m];
+        // if characters match
+        if(text1[n]== text2[m]){
+            return  dp[n][m] = 1+solve(text1, text2, n-1, m-1, dp);
+        }
+        // if they dont, take maximum of both possibilities
+        return dp[n][m] = max(solve(text1, text2, n-1, m, dp), solve(text1, text2, n, m-1, dp));
+    }
+    
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length();
+        int m = text2.length();
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return solve(text1, text2, n-1, m-1, dp);
     }
 };
