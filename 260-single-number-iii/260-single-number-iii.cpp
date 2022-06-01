49f40cca517c70add73aa58fd5f3729b8b417b9b
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-06-02 00:27:24 +0530

    Time: 10 ms (71.95%), Space: 9.8 MB (92.88%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long long n = 0;
+        for(int i=0; i<nums.size(); i++){
+            n = nums[i]^n;
+        }
+        int mask = n & ~(n-1);
+        int xor1 = 0;
+        int xor2 = 0;
+        for(auto i: nums){
+            if(mask & i){
+                xor1 = xor1^i;
+            }
+            else{
+                xor2 = xor2^i;
+            }
+            
+        }
+        return {xor1,xor2};
+    }
+};
