eb601eb938dc4ecbe3aff98652c33ebbcea37d2a
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-06-05 12:57:25 +0530

    Time: 9 ms (47.27%), Space: 6 MB (79.40%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    bool isSafe(int row, int col, vector<string>& vec, int n){
+        int r = row;
+        int c = col;
+        
+        while(row>=0 && col>=0){
+            if(vec[row][col] == 'Q'){
+                return false;
+            }
+            row--, col--;
+        }
+        
+        row = r;
+        col = c;
+        
+        while(col >=0){
+            if(vec[row][col] == 'Q'){
+                return false;
+            }
+            col--;
+        }
+        
+        row = r;
+        col = c;
+        
+        while(row<n && col >=0){
+            if(vec[row][col] == 'Q'){
+                return false;
+            }
+            col--;
+            row++;
+        }
+        
+        return true;
+    }
+    
+    void solve(int n, int& ans, vector<string>& vec, int col){
+        if(col == n){
+            ans++;
+            return;
+        }
+        for(int row = 0; row<n; row++){
+            if(isSafe(row, col, vec, n)){
+                vec[row][col] = 'Q';
+                solve(n, ans, vec, col+1);
+                vec[row][col] = '.';
+            }
+        }
+    }
+    
+    int totalNQueens(int n) {
+        int ans = 0;
+        string st = "";
+        for(int i=0; i<n; i++){
+            st+= ".";
+        }
+        vector<string> vec;
+        for(int i=0; i<n; i++){
+            vec.push_back(st);
+        }
+        
+        solve(n, ans, vec, 0);
+        return ans;
+    }
+};
