cec7e6f5770e2344a1d7fc798664564410732b20
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-06-13 17:44:10 +0530

    Time: 9 ms (49.75%), Space: 8.9 MB (49.02%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        
-        vector<vector<int>> dp = triangle;
-        dp[0][0] = triangle[0][0];
+        vector<vector<int>> vec = triangle;
         
         for(int i=1; i<n; i++){
-            for(int j=0; j<triangle[i].size(); j++){
-                if(j==0){
-                    dp[i][j] = dp[i-1][j] + triangle[i][j];
+            for(int j = 0; j<triangle[i].size(); j++){
+                if(j ==0){
+                    vec[i][j] = vec[i-1][j] + triangle[i][j];
                 }
-                else if(j==triangle[i].size()-1){
-                    dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+                else if(j== triangle[i].size()-1){
+                    vec[i][j] = vec[i-1][j-1] + triangle[i][j];
                 }
                 else{
-                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
+                    vec[i][j] = min(vec[i-1][j], vec[i-1][j-1]) + triangle[i][j];
                 }
             }
         }
+        
         int ans = INT_MAX;
-        for(auto i : dp[n-1]){
+        for(auto i: vec[n-1]){
             ans = min(ans, i);
         }
         return ans;
