7bb2afac8632bc1bdea649c21181caf94313bff5
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-06-14 19:54:12 +0530

    Time: 32 ms (36.50%), Space: 16.6 MB (24.31%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& G) {
+        vector<vector<int>> ans;
+        queue<vector<int>> q;
+        q.push({0});                                                   // starting node of every path
+        while(size(q)) {
+            vector<int> path = q.front(); 
+            q.pop();
+            if(path.back() == size(G)-1) ans.push_back(path);    // found valid path
+            else
+                for(auto child : G[path.back()]) {                     // try every possible next node in path
+                    path.push_back(child);
+                    q.push(path);                                      // push path into queue for further exploration
+                    path.pop_back();
+                }
+        }
+        return ans;
+    }
+};
