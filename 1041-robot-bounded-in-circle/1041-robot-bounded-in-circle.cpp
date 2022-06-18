c76128ad60c86a1bafd1c76dd0db0db72be80e0b
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-06-18 16:54:17 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (52.39%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
     bool isRobotBounded(string instructions) {
-        int x = 0, y = 0, i = 0;
-        vector<vector<int>> d = {{0, 1}, {1, 0}, {0, -1}, { -1, 0}};
-        for (char & ins : instructions)
-            if (ins == 'R')
-                i = (i + 1) % 4;
-            else if (ins == 'L')
-                i = (i + 3) % 4;
-            else
-                x += d[i][0], y += d[i][1];
-        return x == 0 && y == 0 || i > 0;
+        char curr = 'N';
+        int x = 0;
+        int y = 0;
+        for(int i=0; i<instructions.length(); i++){
+            if(instructions[i] == 'G'){
+                y+= curr == 'N' ? 1 : 0;
+                y+= curr == 'S' ? -1: 0;
+                x+= curr == 'E' ? 1:0;
+                x+= curr == 'W' ? -1: 0;
+                
+            }
+            else {
+                char incoming = instructions[i];
+                if(curr == 'N'){
+                    curr = incoming == 'L' ? 'W' : 'E';
+                }
+                else if(curr == 'W'){
+                    curr = incoming == 'L' ? 'S' : 'N';
+                }
+                else if(curr == 'S'){
+                    curr = incoming == 'L' ? 'E' : 'W';
+                }
+                else{
+                    curr = incoming == 'L' ? 'N' : 'S';
+                }
+            }
+        }
+        
+        if(x==0 && y==0 || curr != 'N'){
+            return true;
+        }
+        
+        return false;
     }
 };
