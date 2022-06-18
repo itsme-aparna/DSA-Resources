7b4e5aa5b1d5f1557e41521c329e0d4acd1ddea7
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-06-18 16:35:51 +0530

    Time: 5 ms (20.62%), Space: 6.2 MB (19.96%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -1,47 +1,15 @@
 class Solution {
 public:
-    //returns true if the co-ordinates are at origin
-    bool originVerifier(int x,int y){
-        if(x == 0 && y == 0)
-			return true;
-		return false;
-    }
-	
     bool isRobotBounded(string instructions) {
-        //given
-        char current_dir = 'N';
-        //co-ordinate points
-        int x = 0, y= 0;
-        for(int i=0;i<instructions.length();i++){ 
-            //Case1: If 'G' is in the instruction string
-            if(instructions[i] == 'G'){
-                y += current_dir =='N' ? 1 : 0;
-				y += current_dir =='S' ? -1 : 0;
-				x += current_dir == 'E' ? 1 : 0;
-				x += current_dir == 'W' ? -1 : 0;
-            }
-            //case2: If we found 'directions'
-            else{
-                char incoming = instructions[i];
-                if(current_dir == 'N'){
-                    current_dir = incoming == 'L' ? 'W' : 'E';
-                }
-                else if(current_dir == 'W'){
-                    current_dir = incoming == 'L' ? 'S' : 'N';
-                }
-                else if(current_dir == 'S'){
-                    current_dir = incoming == 'L' ? 'E' : 'W';
-                }
-                else{
-                    current_dir = incoming == 'L' ? 'N' : 'S';
-                }
-            } 
-            
-        }
-        //if the robo is at origin or the current direction is not north than return true
-        if(originVerifier(x,y) || current_dir!='N')
-            return true;
-        
-        return false;
+        int x = 0, y = 0, i = 0;
+        vector<vector<int>> d = {{0, 1}, {1, 0}, {0, -1}, { -1, 0}};
+        for (char & ins : instructions)
+            if (ins == 'R')
+                i = (i + 1) % 4;
+            else if (ins == 'L')
+                i = (i + 3) % 4;
+            else
+                x += d[i][0], y += d[i][1];
+        return x == 0 && y == 0 || i > 0;
     }
 };
