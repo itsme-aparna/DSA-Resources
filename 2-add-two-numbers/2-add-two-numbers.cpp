b9bb4253eb9f379dfa820804e80e3332884a85d0
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-06-30 20:54:29 +0530

    Time: 54 ms (51.18%), Space: 70.9 MB (93.43%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,25 +11,67 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dum = new ListNode(0);
-        ListNode* dummy = dum;
+        
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        
+        
         int carry = 0;
-        while(l1 || l2 || carry){
-            if(l1){
-                carry+= l1->val;
-                l1 = l1->next;
+        ListNode* prev = new ListNode(0);
+        prev->next = l1;
+        while(temp1 && temp2){
+            int sum = temp1->val + temp2->val + carry;
+            if(sum >9){
+                carry = 1;
+                sum = sum-10;
             }
-            if(l2){
-                carry += l2->val;
-                l2 = l2->next;
+            else{
+                carry = 0;
             }
             
-            ListNode* node = new ListNode(carry%10);
-            dummy->next = node;
-            dummy = dummy->next;
-            carry /= 10;
+            temp1->val = sum;
+            temp1 = temp1->next;
+            prev = prev->next;
+            temp2 = temp2->next;
         }
         
-        return dum->next;
+        
+        
+        while(temp1 != NULL){
+            cout<<"addv";
+            int sum = temp1->val + carry;
+            if(sum >9){
+                carry = 1;
+                sum = sum-10;
+            }
+            else{
+                carry = 0;
+            }
+            
+            temp1->val = sum;
+            prev = temp1;
+            temp1= temp1->next;
+        }
+        
+        while(temp2 != NULL){
+            int sum = temp2->val + carry;
+            if(sum >9){
+                carry = 1;
+                sum = sum-10;
+            }
+            else{
+                carry = 0;
+            }
+            temp2->val = sum;
+            prev->next = temp2;
+            prev = prev->next;
+            temp2 = temp2->next;
+        }
+        
+        if(carry == 1){
+            ListNode* node = new ListNode(1);
+            prev->next = node;
+        }
+        return l1;
     }
 };
