f2ecf7102a78a75186d4ef5e0bb2d2601939c854
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-07-11 10:29:15 +0530

    Time: 147 ms (44.14%), Space: 12.1 MB (78.90%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
 	int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-		vector<vector<int>> dp(n,vector<int>(n,INT_MAX/2));
-		for(auto& x:edges){
-			dp[x[0]][x[1]]=x[2];
-			dp[x[1]][x[0]]=x[2];
-		}
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				for(int k=0;k<n;k++){
-					dp[j][k]=min(dp[j][k],dp[j][i]+dp[i][k]);
-				}
-			}
-		}   
-		int ans=-1;
+		
+        vector<vector<int>> dp(n, vector<int>(n, INT_MAX/2));
+        for(auto &i: edges){
+            dp[i[0]][i[1]] = i[2];
+            dp[i[1]][i[0]] = i[2];
+        }
+		for(int i=0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                for(int k = 0; k<n; k++){
+                    dp[j][k] = min(dp[j][k], dp[j][i]+dp[i][k]);
+                }
+            }
+        }
+        
+        int ans=-1;
 		int cur=INT_MAX;
 		for(int i=0;i<n;i++){
 			int count=0;
