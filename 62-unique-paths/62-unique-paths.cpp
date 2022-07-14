385b7d67415b9f382f4513465e98091b7ca6a44c
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-07-14 19:26:45 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-//     //Basic Recursion:-
-//     int uniquePaths(int m, int n) {
-//         if(m==1 || n==1)
-//             return 1;
-        
-//         return uniquePaths(m-1, n) + uniquePaths(m, n-1);
-//     }
-// 1,0; 2,0
     int uniquePaths(int m, int n) {
-        int dp[m+1][n+1];
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         for(int i=0; i<m; i++){
-            dp[i][0] = 1;
-        }
-        for(int i=0; i<n; i++){
-            dp[0][i] = 1;
-        }
-        
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            for(int j=0; j<n; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 1;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                }
             }
         }
         return dp[m-1][n-1];
     }
-    
 };
