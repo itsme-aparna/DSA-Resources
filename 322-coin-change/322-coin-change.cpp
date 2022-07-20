6aaa26690ea9dc86a5afddbb73c8dc717381ba54
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-07-20 16:07:13 +0530

    Time: 193 ms (23.74%), Space: 50.3 MB (7.07%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     
-    int solve(vector<int>& coins, int amount, int idx, vector<vector<int> > &memo){
-       if(amount<=0  || idx >= coins.size()){
-           if(amount == 0){
-               return 0;
-           }
-           else{
-               return INT_MAX-1;
-           }
-           
-       }
-        
-       if(memo[amount][idx] != -1){
-           return memo[amount][idx];
-       }
-        
-       
-        
-        if(amount >= coins[idx])
-            return memo[amount][idx] = min( 1+ solve(coins, amount- coins[idx], idx, memo), solve(coins, amount, idx+1, memo));
-        
-        else
-            return memo[amount][idx] = solve(coins, amount, idx+1, memo);
+    int solve(vector<int> &coins, int amt, vector<vector<int>>& dp, int n){
+        if(n<0 || amt <=0){
+            if(amt==0)
+                return 0;
+            return INT_MAX-1;
+        }
+        if(dp[n][amt]!=-1){
+            return dp[n][amt];
+        }
+        // include
+        if(coins[n]<=amt){
+            return dp[n][amt] = min(solve(coins, amt, dp, n-1), 1+solve(coins, amt-coins[n], dp, n));
+        }
+        // exclude
+        return dp[n][amt] = solve(coins, amt, dp, n-1);
     }
     
     int coinChange(vector<int>& coins, int amount) {
-        // try all combinations
-        // write the recursive code first - done
-        // memoize
-        
-         vector<vector<int> > memo(amount + 1, vector<int> (coins.size(), -1));
-        if(solve(coins, amount, 0, memo)== INT_MAX-1){
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        if(solve(coins, amount, dp, n-1)==INT_MAX-1)
             return -1;
-        }
-        return solve(coins, amount, 0, memo);
+        return solve(coins, amount, dp, n-1);
+        
     }
 };
