81ac4186877766df2722c65969efa74e4bc29c56
Aparna Patra <57916734+itsme-aparna@users.noreply.github.com>
2022-08-01 23:53:37 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        // count the number of ways
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+            for(int j = 0; j<n; j++){
                 if(i==0 || j==0){
                     dp[i][j] = 1;
                 }
-                else{
+                else
                     dp[i][j] = dp[i-1][j]+dp[i][j-1];
-                }
             }
         }
+        
         return dp[m-1][n-1];
     }
 };
